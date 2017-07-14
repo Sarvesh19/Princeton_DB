a2a0e40928ef9cded9c50f777a640a32ce7a0403
Sarvesh19 <sarvesh.y305@gmail.com>
2017-07-14 17:28:02 +0530

    Added two matrix multiplication

--- src/com/corejava/Array_Initialize.java
+++ src/com/corejava/Array_Initialize.java
@@ -3,24 +3,58 @@ package com.corejava;
 import java.util.Scanner;
 
 public class Array_Initialize {
-	public static void main(String[] args) {
-		Integer arr[][] = new Integer[3][4];
-		Scanner sc = new Scanner(System.in);
+	static Integer[][] getFirstMatrix(Scanner sc) {
+		System.out.println("Enter row followd by column :");
+		int row1 = sc.nextInt();
+		int column = sc.nextInt();
+		Integer arr[][] = new Integer[row1][column];
+		System.out.println("Now fill " + row1 + " rows and " + column + " column");
+
 		// System.out.println(arr.length);
 		// System.out.println(arr[1].length);
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[1].length; j++) {
-				arr[i][j] = sc.nextInt();
+		if (arr.length > 1) {
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr[1].length; j++) {
+					arr[i][j] = sc.nextInt();
 
+				}
 			}
+
+			for (int row = 0; row < arr.length; row++) {
+				for (int col = 0; col < arr[1].length; col++) {
+					arr[row][col] = arr[row][col];
+					System.out.print(arr[row][col] + "\t");
+				}
+				System.out.println();
+			}
+		} else {
+			for (int i = 0; i < arr.length; i++) {
+				for (int j = 0; j < arr[0].length; j++) {
+					arr[i][j] = sc.nextInt();
+
+				}
+			}
+
 		}
 
-		for (int row = 0; row < arr.length; row++) {
-			for (int col = 0; col < arr[1].length; col++) {
-				arr[row][col] =arr[row][col];
-				System.out.print(arr[row][col] + "\t");
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		Integer[][] matrix1 = getFirstMatrix(sc);
+		Integer[][] matrix2 = getFirstMatrix(sc);
+		sc.close();
+
+		if (matrix1[0].length == matrix2.length) {
+			for (int i = 0; i < matrix1.length; i++) {
+//				for (int j = 0; j < matrix2[1].length; j++) {
+//
+//				}
 			}
-			System.out.println();
+		} else {
+			System.out.println("First's column not matching with other's row");
 		}
 
 		//
@@ -39,7 +73,5 @@ public class Array_Initialize {
 		// j++;
 		// // }
 		// }
-		System.out.println(arr);
-		sc.close();
 	}
 }
