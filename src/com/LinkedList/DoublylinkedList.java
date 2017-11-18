dd44a05485a76ed92efe4ebcd56ec74f5800df6b
Sarvesh Yadav <sarvesh.y305@gmail.com>
2017-11-18 13:23:19 +0530

    Edited added addLast()

--- src/com/LinkedList/DoublylinkedList.java
+++ src/com/LinkedList/DoublylinkedList.java
@@ -73,8 +73,26 @@ public class DoublylinkedList {
 		return true;
 	}
 
-	public void addLast() {
+	public void addLast(Object o) {
+		Node node = new Node(null, null, o);
+		if (head == null) {
+			head = node;
+		}
 
+		if (tail == null) {
+			tail = head;
+		}
+		Node tailTemp = tail;
+		if(tailTemp !=null){
+			node.previous = tailTemp;
+			tail.next = node;
+			tail = tail.next;
+			node.next = null;
+			
+		}
+		
+		
+		this.modCount++;
 	}
 
 	public void addFirst() {
@@ -91,6 +109,10 @@ public class DoublylinkedList {
 
 	public void remove(Object o) {
 
+	}
+	
+	public boolean contains(Object o){
+		return false;
 	}
 
 	public int size() {
@@ -111,11 +133,16 @@ public class DoublylinkedList {
 		dll.add("drrrr", 3);
 		dll.add("raefeoda");
 		dll.add("www");
+		dll.addLast("bhai me last me hu");
+		dll.addLast("bhai me last me hu ekdum");
+		System.out.println(dll);
 
-		// LinkedList l = new LinkedList();
-		// l.add("rvrv");
-		// l.add("sat");
-		// l.add("145");
+		 LinkedList l = new LinkedList();
+		 l.add("rvrv");
+		 l.add("sat");
+		 l.addLast("pitbull");
+		 l.add("145");
+		 System.out.println(l);
 
 	}
 
