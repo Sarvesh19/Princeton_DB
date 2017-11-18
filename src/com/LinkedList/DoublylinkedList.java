eebfb76e62fbcb15fb58449d6af8f01d45e9b81f
Sarvesh Yadav <sarvesh.y305@gmail.com>
2017-11-18 12:39:01 +0530

    Added doubly linkedlist

---
+++ src/com/LinkedList/DoublylinkedList.java
@@ -0,0 +1,122 @@
+import java.rmi.UnexpectedException;
+import java.util.LinkedList;
+
+public class DoublylinkedList {
+	private Node head;
+	private Node tail;
+	private int modCount = 0;
+	private int count = 0;
+
+	private class Node {
+		private Node next;
+		private Node previous;
+		private Object element;
+
+		public Node(Node next, Node previous, Object element) {
+			super();
+			this.next = next;
+			this.previous = previous;
+			this.element = element;
+		}
+
+	}
+
+	public boolean add(Object o) {
+		Node node = new Node(null, null, o);
+		if (head != null) {
+			tail.next = node;
+			node.previous = tail;
+			tail = node;
+
+		}
+		if (head == null) {
+			head = node;
+		}
+
+		if (tail == null) {
+			tail = head;
+		}
+		this.modCount++;
+		return true;
+	}
+
+	public boolean add(Object o, int pos) throws UnexpectedException {
+		Node node = new Node(null, null, o);
+
+		if (pos > modCount || pos < 0) {
+			throw new UnexpectedException(
+					"Add valid position to add element in list");
+		} else {
+			Node temp = head;
+			for (int i = 0; i < pos; i++) {
+
+				temp = temp.next;
+
+				if (i == pos - 1) {
+					Node afterOfNewBlockNode = temp.next;
+					node.previous = temp;
+					node.next = afterOfNewBlockNode;
+					// node = afterOfNewBlockNode.previous;
+					temp.next = node;
+					afterOfNewBlockNode.previous = node;
+				}
+
+				// node.previous = temp;
+				// temp.next = node;
+
+			}
+			modCount++;
+			// System.out.println("count" + count);
+
+		}
+
+		return true;
+	}
+
+	public void addLast() {
+
+	}
+
+	public void addFirst() {
+
+	}
+
+	public void removeFirst() {
+
+	}
+
+	public void removeLast() {
+
+	}
+
+	public void remove(Object o) {
+
+	}
+
+	public int size() {
+		return modCount;
+	}
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public static void main(String[] args) throws UnexpectedException {
+		DoublylinkedList dll = new DoublylinkedList();
+		dll.add("ccw");
+		dll.add("sarvesh");
+		dll.add("raoda");
+		dll.add("fr");
+		dll.add("eww");
+		dll.add("drrrr", 3);
+		dll.add("raefeoda");
+		dll.add("www");
+
+		// LinkedList l = new LinkedList();
+		// l.add("rvrv");
+		// l.add("sat");
+		// l.add("145");
+
+	}
+
+}
